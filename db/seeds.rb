5a76402159be1989db137721533fecec67925fe2
Benjamin Solis Duran <benjaminsolis@Benjamins-MacBook-Air-2.local>
2025-04-09 15:24:26 -0400

    Agrega nuevas cosas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+10.times do |i|
+    User.create(
+      email: "student#{i}@uandes.cl",
+      first_name: "Student",
+      last_name: "Number#{i}"
+    )
+  end
+  
+  puts "Created #{User.count} users"
+  
+
+  users = User.all
+  10.times do |i|
+    
+    Chat.create(
+      sender_id: users[i].id,
+      receiver_id: users[(i+1) % 10].id 
+    )
+  end
+  
+  puts "Created #{Chat.count} chats"
+  
+
+  chats = Chat.all
+  10.times do |i|
+    chat = chats[i % 10]  
+    
+    Message.create(
+      chat_id: chat.id,
+      user_id: chat.sender_id,  
+      body: "This is message #{i+1} from a student project"
+    )
+  end
+  
+  puts "Created #{Message.count} messages"
