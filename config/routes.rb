1348f2acaacbf2a885df226918bb588112c30a47
Benjamin Solis Duran <benjaminsolis@Benjamins-MacBook-Air-2.local>
2025-05-02 20:00:06 -0400

    Agregadas rutas 'new' y 'create' para chats

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#index' 
-  
+  root 'users#index'
+
   resources :users, only: [:index, :show]
-  resources :chats, only: [:index, :show]
+  
+  # Aquí se agregan las rutas para crear un nuevo chat
+  resources :chats, only: [:index, :show, :new, :create]
+
   resources :messages, only: [:index, :show]
 end
