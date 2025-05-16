b33ba5c29816ba4dba4b7f6dd13f196ca288bf8d
Benjamin Solis Duran <benjaminsolis@Benjamins-MacBook-Air-2.local>
2025-05-16 19:38:18 -0400

    para que se vea mas bonito

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "users#index"
+  get "home/index"
+  root "home#index"
+
 
   resources :users, only: [ :index, :show, :edit, :update ]
   resources :chats, only: [ :index, :show, :new, :create, :edit, :update ]
